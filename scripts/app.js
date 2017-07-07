6b4a4df8375e478107894f731ccf34ed7cabfbf5
Ghizmilth <hidanerferrer@gmail.com>
2017-07-07 15:49:08 -0700

    geoquakes data object retrieved.js

--- scripts/app.js
+++ scripts/app.js
@@ -5,4 +5,31 @@ $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
 
+
 });
+
+
+$.ajax({
+method: 'GET',
+url: 'https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2017-06-30%2000:00:00&endtime=2017-07-07%2023:59:59&minmagnitude=2.5&orderby=time',
+dataType: 'json',
+success: onSuccess
+});
+
+
+  function onSuccess(qData) {
+    console.log(qData);
+
+
+//var quakeList = responseData;
+
+//var dataTitle =
+
+//for
+
+qData.data.forEach(function(v,i) {
+  $(".eqList").append($(`
+    <li>v.data[2].properties.title</li>
+    `));
+})
+}
